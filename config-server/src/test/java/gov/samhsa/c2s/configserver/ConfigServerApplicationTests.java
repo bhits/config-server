786cd050b942f006503ec59e9edd9657df25a445
burcakulug <burcak.ulug@feisystems.com>
2016-09-02 14:37:18 -0400

    Create initial project for config-server
    #10749:As a developer, I can use a centralized configuration management tool, in order to enhance continuous delivery of C2S and the development process [10408]

---
+++ config-server/src/test/java/gov/samhsa/c2s/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,18 @@
+package gov.samhsa.c2s.configserver;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Ignore("No default configuration for GIT")
+public class ConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
